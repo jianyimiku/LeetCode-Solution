4aa64cfcfcc09d5b0fe23bf7e732723883fea8bf
石佳琪 <rt006814@rotai.com>
2022-10-10 09:11:35 +0800

    增加快速排序三种写法

--- src/main/java/org/solution/0001-1000/0215-KthLargestElementinanArray/KthLargestElementinanArrayWithQuickSort.java
+++ src/main/java/org/solution/0001-1000/0215-KthLargestElementinanArray/KthLargestElementinanArrayWithQuickSort.java
@@ -11,8 +11,9 @@ class KthLargestElementinanArrayWithQuickSort {
         int left = 0;
         int right = len - 1;
 
+        // 我们可以知道当pivotIndex小于目标的时候,表示左边都是比pivotIndex小的
         while (true) {
-            int pivotIndex = partition(nums, left, right);
+            int pivotIndex = paritionWithThreeWay(nums, left, right);
             if (pivotIndex == target) {
                 return nums[pivotIndex];
             } else if (pivotIndex < target) {
@@ -24,7 +25,42 @@ class KthLargestElementinanArrayWithQuickSort {
         }
     }
 
-    private int partition(int[] nums, int left, int right) {
+    /**
+     * 单路快速排序
+     *
+     * @param nums
+     * @param left
+     * @param right
+     * @return
+     */
+    private int paritionWithOneWay(int[] nums, int left, int right) {
+        // 防止出现近乎有序的情况 所以需要随机交换第一个元素
+        int randomIndex = left + random.nextInt(right - left + 1);
+        swap(nums, left, randomIndex);
+
+        int temp = nums[left];
+        int low = left;
+        for (int i = left + 1; i <= right; i++) {
+            if (nums[i] < temp) {
+                swap(nums, low + 1, i);
+                low++;
+            }
+        }
+        swap(nums, left, low);
+        return low;
+    }
+
+
+    /**
+     * 双路快排
+     * 确定Parition位置
+     *
+     * @param nums
+     * @param left
+     * @param right
+     * @return
+     */
+    private int paritionWithTwoWay(int[] nums, int left, int right) {
         // 防止出现近乎有序的情况 所以需要随机交换第一个元素
         int randomIndex = left + random.nextInt(right - left + 1);
         swap(nums, left, randomIndex);
@@ -54,6 +90,41 @@ class KthLargestElementinanArrayWithQuickSort {
         return r;
     }
 
+
+    /**
+     * 三路快速排序
+     *
+     * @param nums
+     * @param left
+     * @param right
+     * @return
+     */
+    private int paritionWithThreeWay(int[] nums, int left, int right) {
+        int randowIndex = left + random.nextInt(right - left + 1);
+        swap(nums, left, randowIndex);
+
+        // [left,lt] < v [lt+1,gt) == v [gt...last] > v
+        int temp = nums[left];
+        int lt = left;
+        int i = left + 1;
+        int gt = right + 1;
+        for (int j = i; j < gt; j++) {
+            if (nums[j] < temp) {
+                swap(nums, lt + 1, j);
+                j++;
+                lt++;
+            } else if (nums[j] > temp) {
+                swap(nums, j, gt - 1);
+                j++;
+                gt--;
+            } else {
+                j++;
+            }
+        }
+        swap(nums, left, lt);
+        return lt;
+    }
+
     private void swap(int[] nums, int index1, int index2) {
         int temp = nums[index1];
         nums[index1] = nums[index2];
@@ -64,6 +135,6 @@ class KthLargestElementinanArrayWithQuickSort {
     public static void main(String[] args) {
         KthLargestElementinanArrayWithQuickSort
                 largestElementinanArrayWithQuickSort = new KthLargestElementinanArrayWithQuickSort();
-        System.out.println(largestElementinanArrayWithQuickSort.findKthLargest(new int[]{-1,2,0}, 2));
+        System.out.println(largestElementinanArrayWithQuickSort.findKthLargest(new int[]{-1, 2, 0}, 2));
     }
 }
