13c762f0380c8522bc56329bb72b30cd1c86e2a0
jianyimiku <1936141611@qq.com>
2022-12-26 17:31:18 +0800

    新增题解

---
+++ src/main/java/org/solution/0001-1000/0322-CoinChange/CoinChange.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+@SuppressWarnings("all")
+public class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (i - coins[j] >= 0 && dp[i - coins[j]] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+
+
+    public static void main(String[] args) {
+        CoinChange coinChange = new CoinChange();
+        System.out.println(coinChange.coinChange(new int[]{1, 2, 5}, 11));
+    }
+}
