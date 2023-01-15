d72012717d277e59c9b10c9a04bfe586684df640
jianyimiku <1936141611@qq.com>
2023-01-15 20:17:49 +0800

    新增题解

---
+++ src/main/java/org/solution/0001-1000/0019-RemoveNthNodeFromEndofList/RemoveNthFromEnd.java
@@ -0,0 +1,22 @@
+import org.solution.common.ListNode;
+
+@SuppressWarnings("all")
+public class RemoveNthFromEnd {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode slow = dummyHead;
+        ListNode fast = head;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+        return dummyHead.next;
+    }
+}
---
+++ src/main/java/org/solution/0001-1000/0019-RemoveNthNodeFromEndofList/删除链表倒数第N个节点.md
@@ -0,0 +1,14 @@
+### [删除链表倒数第N个节点](https://leetcode.cn/problems/remove-nth-node-from-end-of-list/)
+给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+### 例子
+![](https://assets.leetcode.com/uploads/2020/10/03/remove_ex1.jpg)
+```text
+输入：head = [1,2,3,4,5], n = 2
+输出：[1,2,3,5]
+```
+```text
+输入：head = [1], n = 1
+输出：[]
+```
+### 解法
+快慢指针
